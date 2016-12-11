86ce65d0224a9f0749db990e8888f80362ac5eb7
esqu1 <brandbest1@gmail.com>
2016-12-11 16:22:11 -0500

    js is lovely for navbar

---
+++ js/script.js
@@ -0,0 +1,19 @@
+new WOW().init();
+$(function(){
+	$.stellar({
+		horizontalScrolling: false,
+	});
+});
+
+$(document).ready(function(){
+	$("li.dropdown").hover(function(){
+		$(this.childNodes[3]).css("visibility","visible"); 
+		var d = this.childNodes[3].getElementsByTagName("a");
+		$(d[d.length - 1]).css("border", "2px solid #c5e8e0");
+		$(d[d.length - 1]).css("border-radius","0px 0px 10px 10px");
+
+		console.log(d);
+	}, function(){
+		$(this.childNodes[3]).css("visibility","hidden");
+	});
+})
