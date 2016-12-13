8a28ac8a65865e5dce2b11ad46337a417537c9b6
esqu1 <brandbest1@gmail.com>
2016-12-12 20:55:21 -0500

    jquery is amazing

--- js/script.js
+++ js/script.js
@@ -5,13 +5,18 @@ $(function(){
 	});
 });
 
-$(document).ready(function(){
+function dropdown(){
 	$("li.dropdown").hover(function(){
-		$(this.childNodes[3]).css("visibility","visible"); 
+		$(this).find(".dropdown-content").fadeToggle(100); 
 		var d = this.childNodes[3].getElementsByTagName("a");
 		$(d[d.length - 1]).css("border", "2px solid #c5e8e0");
 		$(d[d.length - 1]).css("border-radius","0px 0px 10px 10px");
 	}, function(){
-		$(this.childNodes[3]).css("visibility","hidden");
+		$(this).find(".dropdown-content").fadeToggle(100);
 	});
+}
+
+$(document).ready(function(){
+	dropdown();
+	$("li.dropdown .dropdown-content").hide();
 })
